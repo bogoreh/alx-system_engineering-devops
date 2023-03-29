a82070bfb3eca026693fd55e9eeb37b4b36bd851
leroysb <leroybuliro@gmail.com>
2023-03-29 18:58:27 +0300

    adv task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,13 +4,13 @@ package { 'nginx':
   provider => 'apt',
 }
 
-file {
+file { 'Hello World!':
   ensure  => file,
   path    => '/var/www/html/index.nginx-debian.html',
   content => 'Hello World!',
 }
 
-file_line {
+file_line { 'Hello World!':
   path  => '/etc/nginx/sites-available/default',
   after => 'server_name _;',
   line  => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
