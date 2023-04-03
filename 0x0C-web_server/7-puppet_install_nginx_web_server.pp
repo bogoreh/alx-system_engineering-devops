01643b2f24bb5501c535cd82757d720785bc7829
leroysb <leroybuliro@gmail.com>
2023-04-03 15:29:32 +0300

    task5 bug fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,6 @@
 # Installs and configures nginx
-package { 'nginx':
-  ensure   => '1.18.0',
-  provider => 'apt',
-}
 
-file { 'Hello World!':
-  ensure  => file,
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello World!',
-}
-
-file_line { 'Hello World!':
-  path  => '/etc/nginx/sites-available/default',
-  after => 'server_name _;',
-  line  => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-exec { 'service':
-  command  => 'service nginx start',
-  provider => 'bash',
-  user     => 'root',
-  path     => '/usr/sbin/service',
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt -y update ; sudo apt -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.buliro.net permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
